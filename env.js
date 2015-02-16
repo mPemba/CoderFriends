333d5de0d439497d12afa9cb9feb6e60b17378ad
Michael Sayer <michael.j.say@gmail.com>
2015-02-15 19:17:11 -0700

    still working

---
+++ env.js
@@ -0,0 +1,6 @@
+module.exports = {
+	GITHUB: {
+		CLIENT_ID: 'e2cef79827cf628f5d3f',
+		CLIENT_SECRET: '0951f94f8a737b6eb0295f42ec253e92560d7d32'
+	}
+}
--- server.js
+++ server.js
@@ -1,18 +1,42 @@
 var express = require('express'),
-    session = require('express-session'),
+    app = express(),
+    port = 8666,
+    env = require('./env'),
+    bodyParser = require('body-parser'),
     passport = require('passport'),
     githubStrategy = require('passport-github').Strategy,
-    githubApi = require('github');
+    session = require('express-session'),
+    githubApi = require('github'),
+    cors = require('cors');
 
-var app = express();
-var port = 8666;
 
-app.use(session({
-	secret: '7nds94f3∆sas;∆dfsjdf∆'
-}))
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(session({secret: '∆7nsa4325ds94f3∆s2345as;∆d235fsjdf∆'}));
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(express.static(__dirname+'/public'));
+app.use(cors());
+
+passport.use(new githubStrategy({
+  clientID: env.GITHUB.CLIENT_ID,
+  clientSecret: env.GITHUB.CLIENT_SECRET,
+  callbackURL: 'http://localhost:8666/auth/github/callback'
+}, function(accessToken, refreshToken, profile, done) {
+      done(null, profile);
+}))
+
+var isAuthed = function(req, res, next) {
+  if (!req.isAuthenticated()) {
+    return res.status(403).end();
+  } else {
+      next();
+  }
+}
+
+var github = new githubApi({
+  version: "3.0.0"
+})
 
 passport.serializeUser(function(user, done) {
 	done(null, user);
@@ -22,38 +46,39 @@ passport.deserializeUser(function(obj, done) {
 	done(null, obj);
 })
 
-var isAuthed = function(req, res, next) {
-  if (!req.isAuthenticated()) {
-    return res.status(403).end();
-  }
-  return next();
-}
-
-var github = new githubApi({
-	version: "3.0.0"
-})
-
-app.get('/api/github/following', isAuthed, function(req, res) {
-    github.user.getFollowingFromUser({
-    	user: "mPemba"
-    }, function(err, response) {
-    	console.log(response);
-    	res.send(response);
-    })
-})
-
-passport.use(new githubStrategy({
-	clientID: 'e2cef79827cf628f5d3f',
-	clientSecret: '0951f94f8a737b6eb0295f42ec253e92560d7d32',
-	callbackURL: 'http://localhost:8666/auth/github/callback'
-}))
-
 app.get('/auth/github', passport.authenticate('github'));
+app.get('/auth/github/callback', passport.authenticate('github', {failureRedirect: '/login'}), function(req, res) {
+  res.redirect('/#/home');
+}),
+app.get('/api/github/following', isAuthed, function(req, res) {
+  github.user.getFollowingFromUser({
+    user: req.user.username
+  }, function(err, response) {
+    res.json(response);
+  })
+})
 
-app.get('/auth/github/callback', passport.authenticate('github', {
-	successRedirect: '/home',
-	failureRedirect: '/failure'
-}))
+// app.get('/auth/github/callback', passport.authenticate('github', {
+//   successRedirect: '/#/home',
+//   failureRedirect: '/failure'
+// }))
+
+// app.get('/api/github/following', isAuthed, function(req, res) {
+//     github.user.getFollowingFromUser({
+//     	user: req.user.username
+//     }, function(err, response) {
+//     	console.log(response);
+//     	res.json(response);
+//     })
+// })
+
+app.get('/api/github/:username/activity', isAuthed, function(req, res) {
+  github.events.getFromUser({
+    user: req.params.username
+  }, function(err, response) {
+      res.json(response)
+  })
+})
 
 app.listen(port, function() {
 	console.log('now listening on port ' + port);
